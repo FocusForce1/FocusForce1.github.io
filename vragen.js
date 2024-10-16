420fa5f5f3ac7e8b8d7232b157ae902a4d52beac
FocusForce1 <1100414@hr.nl>
2024-10-16 10:43:12 +0200

    meer images

--- vragen.js
+++ vragen.js
@@ -66,6 +66,9 @@ const adhdImages = [
     '/img/img3.png',
     '/img/img4.png',
     '/img/img5.png',
+    '/img/img6.png',
+    '/img/img7.png',
+    '/img/img8.png',
 ];
  
 const usedQuestionsQuiz1 = [];
